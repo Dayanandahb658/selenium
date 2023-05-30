d8f9aeb5a4fc6cef9e426a419844bd22aad3484a
Dell i7 16 <Dell i7 16@DESKTOP-TCJQPBH>
2023-05-30 17:46:21 +0530

    push

--- Selenium/src/Testing/Demo1.java
+++ Selenium/src/Testing/Demo1.java
@@ -1,13 +1,51 @@
 package Testing;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 //port org.testng.annotations.Test;
 
 public class Demo1
  {
 	
-  public static void main(String[] args) 
-	
-  {
-  System.out.println("jkjkjuui");
-  }
-}
+//  public static void main(String[] args) 
+//	
+//  {
+//  System.out.println("jkjkjuui");
+//  }
+//}
+	@Test
+	public void Best1() 
+	{
+	 System.out.println("Best1");
+	}
+	@BeforeTest
+	public void Best2() 
+	{
+		System.out.println("Best2");
+	}
+	@BeforeClass
+	public void Best3() 
+	{
+		System.out.println("Best3");
+	}
+	@BeforeGroups
+	public void Best4() 
+	{
+		System.out.println("Best4");
+	}
+     @BeforeMethod
+	public void Best5() 
+	{
+		System.out.println("Best5");
+	}
+     @BeforeSuite
+ 	public void Best6() 
+ 	{
+ 		System.out.println("Best6");
+ 	}
+	}
